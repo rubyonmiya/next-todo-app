34bce39885a09535e84c3acd49f7a4156891844f
rubyonmiya <ikkiiimonkey0229@gmail.com>
2022-09-25 00:16:10 +0900

    importを一部import typeに修正

--- src/components/layouts.tsx
+++ src/components/layouts.tsx
@@ -1,6 +1,6 @@
 import MenuIcon from '@mui/icons-material/Menu'
 import { AppBar, Box, Button, IconButton, Toolbar, Typography } from '@mui/material'
-import { ReactNode } from 'react'
+import type { ReactNode } from 'react'
 
 type Props = {
   children: ReactNode
